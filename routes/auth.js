376960f27a47e3c78219943eaa976c642da3fb61
ShragaUser <dandankidon@gmail.com>
2019-04-18 11:15:34 -0400

    added enrichUser function

--- routes/auth.js
+++ routes/auth.js
@@ -13,11 +13,20 @@ router.post("/saml", passport.authenticate("saml"), (req, res) => {
         user
     } = req;
 
+    user = enrichUser(user);
+
     let jwt = nJwt.create(user, Buffer.from(req.cookies["SignInSecret"], 'base64'));
     res.cookie('jwtUserCreds', jwt.compact());
 
     res.redirect(307, req.cookies["callbackURL"]);
 });
+
+function enrichUser(user) {
+    if (process.env.enrichUrl) {
+        // enrich
+    }
+    return user;
+}
 
 
 module.exports = router;
