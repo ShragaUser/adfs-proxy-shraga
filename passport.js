a3aa53f697339464d09dac40a3a256200018004d
omri haber <omrihaber@me.com>
2019-05-12 23:44:28 +0300

    @feature/profileExtractor  can be provided as a function or a mapper object

--- passport.js
+++ passport.js
@@ -3,6 +3,17 @@ const { Strategy: SamlStrategy } = require("passport-saml");
 const dotenv = require("dotenv");
 const authConfig = require("./authConfig");
 
+//checks if func is a function
+const isFunction = (func)=>func instanceof Function;
+//extracts claims from profile by profileExtractor object keys
+const extract = (profile,profileExtractor)=>{
+  let res = {}
+  Object.keys(profileExtractor).forEach(key=>
+    res[key]=profile[profileExtractor[key]]
+  )
+  return res;
+}
+
 dotenv.config();
 
 let users = [];
@@ -23,13 +34,12 @@ const configurePassport = db => {
   const { saml: samlConfig, profileExtractor } = authConfig();
   passport.use(
     new SamlStrategy(samlConfig, (profile, done) => {
-      profile = {
-        id: profile[profileExtractor.id],
-        firstName: profile[profileExtractor.firstName],
-        lastName: profile[profileExtractor.lastName],
-        displayName: profile[profileExtractor.displayName],
-        mail: profile[profileExtractor.mail]
-      };
+      //if profileExtractor is provided as a function use it to extract profile
+      //else assume its an object whos keys are the profiles keys and the values are the keys in
+      //the provided profile from saml
+      profile = isFunction(profileExtractor)?
+      profileExtractor(profile)
+      :extract(profile,profileExtractor)
       if (users.filter(user => user.id === profile.id).length === 0) {
         users.push(profile);
       }
