5091488ef53ba4ad3738ae6adf9ff38a212cf0cd
ShragaUser <dandankidon@gmail.com>
2019-05-20 03:04:18 -0400

    fix(cors handling): works over http not cookie.
    jwtCreds sent with query params on res.redirect
    jwtSign recieved via http request.

--- routes/auth.js
+++ routes/auth.js
@@ -4,9 +4,11 @@ const nJwt = require("njwt");
 
 const authConfig = require("../authConfig");
 
-const {enrichment} = authConfig();
+const {
+    enrichment
+} = authConfig();
 //by default enrich is the identity function if undefined
-const enrich = enrichment.enrich?enrichment.enrich:x=>x;
+const enrich = enrichment.enrich ? enrichment.enrich : x => x;
 const router = Router();
 
 router.get("/saml", passport.authenticate("saml"), (req, res) => {
@@ -23,7 +25,7 @@ router.post("/saml", passport.authenticate("saml"), (req, res) => {
     let jwt = nJwt.create(user, Buffer.from(req.cookies["SignInSecret"], 'base64'));
     res.cookie('jwtUserCreds', jwt.compact());
 
-    res.redirect(307, req.cookies["callbackURL"]);
+    res.redirect(307, `${req.cookies["callbackURL"]}/?jwt=${jwt.compact()}`);
 });
 
 
--- routes/index.js
+++ routes/index.js
@@ -1,22 +1,21 @@
 var express = require('express');
 var router = express.Router();
 const axios = require("axios");
-const secureRandom = require('secure-random');
-
-const generateRandomBuffer = ()=>secureRandom(256, {
-  type: 'Buffer'
-});
-const generateBase64SigningKey= ()=>generateRandomBuffer().toString('base64');
 
 router.get('/', function (req, res, next) {
   axios.post(req.cookies["callbackURL"]).then(() => console.log('s')).catch((err) => console.error(err));
 });
 
 router.get('/setCallback/:callbackURL', function (req, res, next) {
-  const { callbackURL } = req.params;
-  const base64SigningKey = generateBase64SigningKey()
+  const {
+    callbackURL
+  } = req.params;
+  const {
+    SignInSecret
+  } = req.query;
+
   res.cookie("callbackURL", callbackURL);
-  res.cookie("SignInSecret", base64SigningKey);
+  res.cookie("SignInSecret", SignInSecret);
   res.status(200).redirect('/auth/saml');
 });
 
